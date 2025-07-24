c3e5347a39bb962a19f30eaba32f8316de3f5faa
Callum Fortune <callumfortune03@outlook.com>
2025-07-25 00:15:35 +0100

    icon

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,6 +11,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
+
 document.querySelector('meta[name="theme-color"]')?.setAttribute('content', '#030712');
 
 
